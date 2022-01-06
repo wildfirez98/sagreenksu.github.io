3dea549be50b878a80b03a2425b4438bebef01bb
Scott Green <sagreenxyz@gmail.com>
2022-01-06 13:24:56 -0600

    Updating content

--- script-update-content.sh
+++
@@ -1,8 +0,0 @@
-echo "Running..."
-git add -A
-echo "git add -A"
-git commit -m "Script Updating Content"
-echo "git commit -m 'Script Updating Content'"
-git push origin main
-echo "git push origin main"
-echo "Complete!"
